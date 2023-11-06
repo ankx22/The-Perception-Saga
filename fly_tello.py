2343f9ba5ab825333df87098c26fe77ff81f69c5
Ankit Talele <ankit.talele76@gmail.com>
2023-11-06 10:42:35 -0500

    Flying through all 3 windows of test env.

--- fly_tello.py
+++ fly_tello.py
@@ -13,8 +13,15 @@ from ultralytics import YOLO
 def draw_axis(img, rotvec, tvec, K):
     # unit is mm
     # rotvec, _ = cv2.Rodrigues(R)
+    dist_coeffs = np.zeros((5,1))
+    #Nov 5 calib 
+    dist_coeffs[0][0] = 0.02456386593401987
+    dist_coeffs[1][0] = -0.5958069654037562
+    dist_coeffs[2][0] = -0.0003932676388405013
+    dist_coeffs[3][0] = -0.00017064279541258975
+    dist_coeffs[4][0] = 1.8486532081847153
     points = np.float32([[20, 0, 0], [0, 20, 0], [0, 0, 20], [0, 0, 0]]).reshape(-1, 3)
-    axisPoints, _ = cv2.projectPoints(points, rotvec, tvec, K, (0, 0, 0, 0))
+    axisPoints, _ = cv2.projectPoints(points, rotvec, tvec, K, dist_coeffs)
     axisPoints = axisPoints.astype(int)
     # print("printing axis points",)
     img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[0].ravel()), (255,0,0), 3) # Blue is x axis
@@ -51,10 +58,10 @@ def get_corners(img, mask):
     img_erosion = cv2.erode(mask, kernel, iterations=15) 
     img_dilation = cv2.dilate(img_erosion, kernel, iterations=15) 
 
-    img_resized = resize_with_aspect_ratio(img, width=900)
-    resized_image = resize_with_aspect_ratio(mask, width=900) # You can adjust the width as needed
-    resized_image_erosion = resize_with_aspect_ratio(img_erosion, width=900) # You can adjust the width as needed
-    resized_image_dilation = resize_with_aspect_ratio(img_dilation, width=900) # You can adjust the width as needed
+    img_resized = resize_with_aspect_ratio(img, width=960)
+    resized_image = resize_with_aspect_ratio(mask, width=960) # You can adjust the width as needed
+    resized_image_erosion = resize_with_aspect_ratio(img_erosion, width=960) # You can adjust the width as needed
+    resized_image_dilation = resize_with_aspect_ratio(img_dilation, width=960) # You can adjust the width as needed
 
     contours, _ = cv2.findContours(np.uint8(resized_image_dilation), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -95,6 +102,7 @@ def get_corners(img, mask):
 def order_points(points):
     # Sort points by x-coordinate (leftmost will be top-left, rightmost will be top-right)
     sorted_points = sorted(points, key=lambda x: x[0])
+    print("sorted func output",sorted_points)
     
     # left most and right most points
     left1 = sorted_points[0]
@@ -104,32 +112,37 @@ def order_points(points):
     right2 = sorted_points[-2]
 
     if left1[1] > left2[1]:
-        top_left = left1
+        bottom_left = left1
+        top_left = left2
+    else:
         bottom_left = left2
-    else:
-        top_left = left2
-
-    if left1[1] > left2[1]:
         top_left = left1
+
+
+    if right1[1] > right2[1]:
+        top_right = right2
+        bottom_right = right1
     else:
-        top_left = left2
-    # Identify top-left and top-right points
-    top_left = sorted_points[0]
-    top_right = sorted_points[1]
+        top_right = right1
+        bottom_right = right2
+    # # Identify top-left and top-right points
+    # top_left = sorted_points[0]
+    # top_right = sorted_points[1]
     
     # Calculate the distance between the top-left and top-right points
     # The point with smaller y-coordinate is top-left, the other is top-right
-    if top_left[1] > top_right[1]:
-        top_left, top_right = top_right, top_left
+    # if top_left[1] > top_right[1]:
+    #     top_left, top_right = top_right, top_left
     
-    # Identify bottom-left and bottom-right points
-    bottom_left = sorted_points[2] if sorted_points[2][1] > sorted_points[3][1] else sorted_points[3]
-    bottom_right = sorted_points[3] if sorted_points[2][1] > sorted_points[3][1] else sorted_points[2]
+    # # Identify bottom-left and bottom-right points
+    # bottom_left = sorted_points[2] if sorted_points[2][1] > sorted_points[3][1] else sorted_points[3]
+    # bottom_right = sorted_points[3] if sorted_points[2][1] > sorted_points[3][1] else sorted_points[2]
     
-    return (top_left, bottom_left, bottom_right, top_right)
+    return [top_left, bottom_left, bottom_right, top_right]
 
 def get_axis(img,corners):
-    K = np.array([[1381.2153584922428,0.0,655.1643687241123],[0.0,1388.0303218031256,360.7844112495259],[0.0,0.0,1.0]])
+    # K = np.array([[1381.2153584922428,0.0,655.1643687241123],[0.0,1388.0303218031256,360.7844112495259],[0.0,0.0,1.0]])
+    K = np.array([[917.3527180617801,0.0,480.97134568905716],[0.0,917.1043451426654,365.57078783755276],[0.0,0.0,1.0]])
     # points_2D = np.array([(128,233),(208,208),(239,320),(158,340)], dtype="double")
     points_2D = np.array([corners], dtype="double")
     points_3D = np.array([
@@ -138,7 +151,13 @@ def get_axis(img,corners):
                         (50.8 ,-45.72,0),# Third
                         (50.8 ,45.72,0) # Fourth 
                         ])
-    dist_coeffs = np.zeros((4,1))
+    dist_coeffs = np.zeros((5,1))
+    #Nov 5 calib 
+    dist_coeffs[0][0] = 0.02456386593401987
+    dist_coeffs[1][0] = -0.5958069654037562
+    dist_coeffs[2][0] = -0.0003932676388405013
+    dist_coeffs[3][0] = -0.00017064279541258975
+    dist_coeffs[4][0] = 1.8486532081847153
     success, rotation_vector, translation_vector = cv2.solvePnP(points_3D, points_2D, K, dist_coeffs, flags=0)
     image = draw_axis(img,rotation_vector,translation_vector,K)
     axis_filename = current_path + str("axes/") + f"frame{i}.png"
@@ -171,81 +190,105 @@ try:
     print('Altitude ', drone.get_distance_tof())
     print('Battery, ', drone.get_battery())
 
-    # drone.takeoff()
-    # time.sleep(2)
-    # drone.move_forward(30)
-    # time.sleep(2)
-    # drone.move_right(30)
-    # time.sleep(2)
-    # drone.move_back(30)
-    # drone.move_back(30)
-    # drone.move_back(30)
-    # drone.move_back(30)
-    # time.sleep(2)
-    # drone.move_left(30)
-    # time.sleep(2)
-    # drone.land()
-
+    
+    center = []
     drone.streamon()
-    # time.sleep(2)
-    # drone.takeoff()
+    drone.takeoff()
+    # frame = drone.get_frame_read().frame
+    # Go to a initial location
+    speed = 45
+    # drone.go_xyz_speed(0,-100,100,speed)
+    counter = 0
     i = 0
-    while True:
-        frame = drone.get_frame_read().frame
-        frame = cv2.resize(frame, (360, 240)) # Windows
-        # frame = cv2.resize(frame, (360, 240), interpolation=cv2.INTER_LINEAR) # Ubuntu
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        H, W, _ = frame.shape
-        print("shape",(H,W))
-        # cv2.imread(frame)
-        filename = current_path + str("/frames/") + f"frame{i}.png"
-        cv2.imwrite(filename, frame)
-        # Running the model on the saved frame
-        # # To run the model
-        frame = cv2.imread(filename)
-        results = model(frame)
-        try:
-            mask = results[0].masks.data
-            mask = mask.cpu().numpy()*255
-            print("mask shape",mask.shape)
-            mask = cv2.resize(mask[0], (W, H))
-            mask_filename = current_path + str("/masks/") + f"frame{i}.png"
-            cv2.imwrite(mask_filename, mask)
+    wp_list = [(0,-65,80),(0,120,0),(0,-105,0)]
+    wp_list = [(0,-75,80),(0,130,0),(0,-115,0)]
+    # groundtruth_list = [(223,0,0),(174,0,0),(202,0,0)]
+    groundtruth_list = [(223,0,0),(194,0,0),(222,0,0)]
+    while counter<3:
+        time.sleep(2)
+        # Send command to predefined waypoint
+        drone.go_xyz_speed(wp_list[counter][0],wp_list[counter][1],wp_list[counter][2],speed) 
+        # drone.go_xyz_speed(183,0,0,speed)
+        # drone.land() 
 
-            # To get corners
-            corners, corner_img = get_corners(frame,mask)
-            rvec, tvec = get_axis(corner_img,corners)
-            # Adding axis to drone and saving image
-
-            i += 1
-        except:
-            continue
-
+        # drone.go_xyz_speed(0,-20,70,speed) 
+        # drone.go_xyz_speed(0,30,-40,speed) 
+        # drone.go_xyz_speed(30,140,0,speed)
+        # drone.go_xyz_speed(150,0,0,speed)
         
+        while True:
 
-        # cv2.imshow('video', frame)
-        # key = cv2.waitKey(2) & 0xFF
-        # if key == ord('q'):
-        #     break
-        # elif key == ord('w'):
-        #     drone.move_forward(30)
-        # elif key == ord('s'):
-        #     drone.move_back(30)
-        # elif key == ord('a'):
-        #     drone.move_left(30)
-        # elif key == ord('d'):
-        #     drone.move_right(30)
-        # elif key == ord('p'):
-        #     print(f"Taking picture....\n")
-        #     filename = f"frame{i}.png"
-        #     cv2.imwrite(filename, frame)
-        
-    # print(f"Landing....")
+            # frame = cv2.resize(frame, (360, 240)) # Windows
+            # frame = cv2.resize(frame, (360, 240), interpolation=cv2.INTER_LINEAR) # Ubuntu
+            frame = drone.get_frame_read().frame
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            H, W, _ = frame.shape
+            print("shape",(H,W))
+            # cv2.imread(frame)
+            filename = current_path + str("/frames/") + f"frame{i}.png"
+            cv2.imwrite(filename, frame)
+            # Running the model on the saved frame
+            # # To run the model
+            frame = cv2.imread(filename)
+            results = model(frame)
+            try:
+                mask = results[0].masks.data
+                mask = mask.cpu().numpy()*255
+                print("mask shape",mask.shape)
+                mask = cv2.resize(mask[0], (W, H))
+                mask_filename = current_path + str("/masks/") + f"frame{i}.png"
+                print(f"wrote {counter}th frame")
+                cv2.imwrite(mask_filename, mask)
+
+                # To get corners
+                corners, corner_img = get_corners(frame,mask)
+                print("corners before ordering",corners)
+                # Reorder the corners
+                corners = order_points(corners)
+                print("corners after:",corners)
+                rvec, tvec = get_axis(corner_img,corners)
+                print("tvec",tvec)
+                if True:
+                    # drone.go_xyz_speed(int(tvec[2][0])+20,int(tvec[1][0])+15,0*int(tvec[0][0]),speed)
+                    drone.go_xyz_speed(int((tvec[2][0]+groundtruth_list[counter][0])/2),int((tvec[1][0] + 0)/2),int((tvec[0][0]+0)/2)-10,speed)
+                    print("went through window ", counter)
+                    i += 1
+                    break
+                # drone.go_xyz_speed(0,100,-100,speed)
+                # drone.go_xyz_speed(int(tvec[2][0]),int(tvec[0][0]),int(tvec[1][0]),speed)
+                # center.append([int(tvec[2][0]),int(tvec[0][0]),int(tvec[1][0])])
+                # print("I came here")
+                # if abs(int(tvec[0][0])) <10
+                # print("sent command to the drone")
+                # Adding axis to drone and saving image
+                # i += 1
+                # print("value of i",i)
+            except:
+                # filename = current_path + str("/frames/") + f"frame{i}.png"
+                # cv2.imwrite(filename, frame)
+                # i+=1
+                continue
+
+            print(center)
+        print(f"Going to next waypoint...")
+        counter+=1
     # drone.land()
 
 except KeyboardInterrupt:
+    # drone.land()
     # HANDLE KEYBOARD INTERRUPT AND STOP THE DRONE COMMANDS
     print('keyboard interrupt')
     drone.emergency()
     drone.emergency()
     drone.end()
+
+# Get the waypoints sent to the drone properly
+# Z axis is not reliable
+# Check all axis once (drone, camera, world)
+# Use map instead of window lenght
+#--------------------------------------
+# Automate the map file reading
+# Tuning parameters
+# Need continuous frames for good video
+# Change window config to check robustness
+# Good visualizations (best)
